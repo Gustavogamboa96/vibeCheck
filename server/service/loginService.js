957ab226e6ec1390184753b3189946e94ab77873
LSierraOsorio <luis.asierraosorio99@gmail.com>
2024-10-01 21:04:39 -0700

    wrote unit tests for loginService.js

--- server/service/loginService.js
+++ server/service/loginService.js
@@ -1,7 +1,8 @@
 const { dataResponse } = require("../utils/dataResponse");
 const userDAO = require("../repository/userDAO");
 const jwt = require("jsonwebtoken");
-require('dotenv').config()
+const bcrypt = require('bcrypt');
+require("dotenv").config()
 
 async function login(username, password) {
     /**
@@ -17,39 +18,36 @@ async function login(username, password) {
         // repository layer function call to get user by username
         const returnedUsers = await userDAO.getUserByUsername(username);
 
-        console.log(returnedUsers);
-
-        // block checks if no user was found, if found then compares passwords
-        if (returnedUsers.Count === 0) {
-            data.message = `No user data found.`;
-            data.registerURL = "someurl";
-        } else {
-            // since usernames should be unique, only 1 JSOn object should be present in the returnedUser
-            const databasePassword = returnedUsers.Items[0].password;
-
-            // block checks that passwords match
-            if (databasePassword !== password) {
-                data.message = "Username and/or Password do not match!"
-                return dataResponse(401, "fail", data);
-            } else {
-                // array method to remove sensitive data from the returning data
-                const newData = returnedUsers.Items.map(user => {
-                    const { password, ...rest } = user;
-                    return rest
-                });
-
-                // console.log(newData);
-
-                // creating token with user info, newData should be an array of a single object
-                let token = jwt.sign(newData[0], process.env.SECRET_KEY, {
-                    expiresIn: "1d"
-                })
-                data.token = token;
-            }
+        // block checks if user does NOT exists
+        if (returnedUsers.Count === 0 || returnedUsers.Items.length === 0) {
+            data.message = "Username and/or Password do not match!";
+            return dataResponse(401, "fail", data);
         }
 
-        return dataResponse(200, "success", data);
+        // since usernames should be unique, only 1 JSOn object should be present in the returnedUser
+        const databasePassword = returnedUsers.Items[0].password;
 
+        // block checks if passwords do NOT match
+        if (!bcrypt.compareSync(databasePassword, password)) {
+            data.message = "Username and/or Password do not match!"
+            return dataResponse(401, "fail", data);
+        }
+
+        // array method to remove sensitive data from the returning data
+        const newData = returnedUsers.Items.map(user => {
+            // destructure password
+            const { password, ...rest } = user;
+            return rest
+        });
+
+        // creating token with user info, newData should be an array of a single object
+        let token = jwt.sign(newData[0], process.env.SECRET_KEY, {
+            expiresIn: "1d"
+        })
+
+        // creating and returning the token
+        data.token = token;
+        return dataResponse(200, "success", data);
     } catch (error) {
         throw new Error(error.message);
     }
